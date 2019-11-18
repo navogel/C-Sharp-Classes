8a95282a3e29f68fd8260cb2ed5a0808ecfcccad
Nate V <navogel@gmail.com>
2019-11-18 10:58:30 -0600

    method for changing classes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassIntro
 {
@@ -6,9 +7,49 @@ namespace ClassIntro
     {
         static void Main(string[] args)
         {
-            Car someCar = new Car();
-            someCar.Make = "ford";
-            Console.WriteLine(someCar.make);
+            List<Car> carLot = new List<Car>();
+            Car ford = new Car();
+            ford.Make = "Ford";
+            ford.Model = "Bronco";
+            ford.Year = 1986;
+            ford.Color = "Pearl";
+            ford.Mileage = 80_000;
+            ford.IsInspected = true;
+            ford.CountryOfORigin = "USA";
+
+            Car toyota = new Car()
+            {
+                Make = "Toyota",
+                Model = "4Runner",
+                Year = 2019,
+                Color = "white",
+                Mileage = 300,
+                IsInspected = false,
+                CountryOfORigin = "Japan"
+            };
+
+            Car chevy = new Car()
+            {
+                Make = "chevy",
+                Model = "mali",
+                Year = 2016,
+                Color = "pink",
+                Mileage = 300000,
+                IsInspected = false,
+                CountryOfORigin = "USA"
+            };
+             
+            carLot.Add(ford);
+            carLot.Add(toyota);
+            carLot.Add(chevy);
+
+            Console.WriteLine($"The ford has {ford.Mileage} miles");
+            ford.Drive(10);
+            Console.WriteLine($"The ford now has {ford.Mileage} miles");
+
+
+        
+            
         }
     }
 }
